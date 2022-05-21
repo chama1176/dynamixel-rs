6264a330a4aacc98fcf2d4ab90b811b5b29b725b
kaede <kaede6120@gmail.com>
2022-05-21 16:09:54 +0900

    関数名の検討中

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ use heapless::Vec;
 use core::result::Result;
 use control_table::ControlTable;
 use instruction::Instruction;
+use packet_handler::MAX_PACKET_LEN;
 
 pub trait Interface {
     fn write_byte(&mut self, data: u8);
@@ -17,6 +18,7 @@ pub trait Interface {
 pub struct DynamixelControl<'a> {
     uart: &'a mut dyn Interface,
     is_enabled: bool,
+    
 }
 
 impl<'a> DynamixelControl<'a> {
@@ -41,16 +43,13 @@ impl<'a> DynamixelControl<'a> {
     /// 👺待ち方が不十分
     pub fn ping(&mut self, id: u8) -> (u16, u8) {
         let length: u16 = 1 + 2;     // instruction + crc
-        let mut msg = Vec::<u8, 256>::new();
-        msg.extend(self.make_msg_header().iter().cloned());
+        let mut msg = Vec::<u8, MAX_PACKET_LEN>::new();
+        msg.extend(self.reserve_msg_header().iter().cloned());
         msg.push(id).unwrap();
         msg.extend(length.to_le_bytes().iter().cloned());       // Set length temporary
         msg.push(Instruction::Ping.to_value()).unwrap();
-        msg.extend(self.calc_crc_value(&msg).to_le_bytes().iter().cloned());
-
-        for m in msg {
-            self.uart.write_byte(m);
-        }
+        
+        self.send_packet(msg);
 
         let mut status = Vec::<u8, 128>::new();
         let status_len = 14;
@@ -72,10 +71,16 @@ impl<'a> DynamixelControl<'a> {
 
     }
 
+    pub fn broadcast_ping(&mut self) {}
+    pub fn action(&mut self) {}
+    pub fn reboot(&mut self) {}
+    pub fn clear_multi_turn(&mut self) {}
+    pub fn factory_reset(&mut self) {}
+
     pub fn write(&mut self, id: u8, address: u16, data: &[u8]) {
         let length: u16 = 1 + 2 + (data.len() as u16) + 2;     // instruction + adress + data + crc
-        let mut msg = Vec::<u8, 256>::new();
-        msg.extend(self.make_msg_header().iter().cloned());
+        let mut msg = Vec::<u8, MAX_PACKET_LEN>::new();
+        msg.extend(self.reserve_msg_header().iter().cloned());
         msg.push(id).unwrap();
         msg.extend(length.to_le_bytes().iter().cloned());       // Set length temporary
         msg.push(Instruction::Write.to_value()).unwrap();
@@ -85,64 +90,14 @@ impl<'a> DynamixelControl<'a> {
             msg.push(*d).unwrap();
         }
 
-        msg.extend(self.calc_crc_value(&msg).to_le_bytes().iter().cloned());
-
-        for m in msg {
-            self.uart.write_byte(m);
-        }
+        self.send_packet(msg);
         // self.uart.read()
     }
 
-    fn make_msg_header(&self) -> [u8; 4] {
-        [0xFF, 0xFF, 0xFD, 0x00]     // Header and reserved
+    fn reserve_msg_header(&self) -> [u8; 4] {
+        [0x00; 4]     // Header and reserved len
     }
 
-    fn calc_crc_value(&self, msg: &Vec<u8, 256>) -> u16 {
-        
-        let crc_table = [
-            0x0000, 0x8005, 0x800F, 0x000A, 0x801B, 0x001E, 0x0014, 0x8011,
-            0x8033, 0x0036, 0x003C, 0x8039, 0x0028, 0x802D, 0x8027, 0x0022,
-            0x8063, 0x0066, 0x006C, 0x8069, 0x0078, 0x807D, 0x8077, 0x0072,
-            0x0050, 0x8055, 0x805F, 0x005A, 0x804B, 0x004E, 0x0044, 0x8041,
-            0x80C3, 0x00C6, 0x00CC, 0x80C9, 0x00D8, 0x80DD, 0x80D7, 0x00D2,
-            0x00F0, 0x80F5, 0x80FF, 0x00FA, 0x80EB, 0x00EE, 0x00E4, 0x80E1,
-            0x00A0, 0x80A5, 0x80AF, 0x00AA, 0x80BB, 0x00BE, 0x00B4, 0x80B1,
-            0x8093, 0x0096, 0x009C, 0x8099, 0x0088, 0x808D, 0x8087, 0x0082,
-            0x8183, 0x0186, 0x018C, 0x8189, 0x0198, 0x819D, 0x8197, 0x0192,
-            0x01B0, 0x81B5, 0x81BF, 0x01BA, 0x81AB, 0x01AE, 0x01A4, 0x81A1,
-            0x01E0, 0x81E5, 0x81EF, 0x01EA, 0x81FB, 0x01FE, 0x01F4, 0x81F1,
-            0x81D3, 0x01D6, 0x01DC, 0x81D9, 0x01C8, 0x81CD, 0x81C7, 0x01C2,
-            0x0140, 0x8145, 0x814F, 0x014A, 0x815B, 0x015E, 0x0154, 0x8151,
-            0x8173, 0x0176, 0x017C, 0x8179, 0x0168, 0x816D, 0x8167, 0x0162,
-            0x8123, 0x0126, 0x012C, 0x8129, 0x0138, 0x813D, 0x8137, 0x0132,
-            0x0110, 0x8115, 0x811F, 0x011A, 0x810B, 0x010E, 0x0104, 0x8101,
-            0x8303, 0x0306, 0x030C, 0x8309, 0x0318, 0x831D, 0x8317, 0x0312,
-            0x0330, 0x8335, 0x833F, 0x033A, 0x832B, 0x032E, 0x0324, 0x8321,
-            0x0360, 0x8365, 0x836F, 0x036A, 0x837B, 0x037E, 0x0374, 0x8371,
-            0x8353, 0x0356, 0x035C, 0x8359, 0x0348, 0x834D, 0x8347, 0x0342,
-            0x03C0, 0x83C5, 0x83CF, 0x03CA, 0x83DB, 0x03DE, 0x03D4, 0x83D1,
-            0x83F3, 0x03F6, 0x03FC, 0x83F9, 0x03E8, 0x83ED, 0x83E7, 0x03E2,
-            0x83A3, 0x03A6, 0x03AC, 0x83A9, 0x03B8, 0x83BD, 0x83B7, 0x03B2,
-            0x0390, 0x8395, 0x839F, 0x039A, 0x838B, 0x038E, 0x0384, 0x8381,
-            0x0280, 0x8285, 0x828F, 0x028A, 0x829B, 0x029E, 0x0294, 0x8291,
-            0x82B3, 0x02B6, 0x02BC, 0x82B9, 0x02A8, 0x82AD, 0x82A7, 0x02A2,
-            0x82E3, 0x02E6, 0x02EC, 0x82E9, 0x02F8, 0x82FD, 0x82F7, 0x02F2,
-            0x02D0, 0x82D5, 0x82DF, 0x02DA, 0x82CB, 0x02CE, 0x02C4, 0x82C1,
-            0x8243, 0x0246, 0x024C, 0x8249, 0x0258, 0x825D, 0x8257, 0x0252,
-            0x0270, 0x8275, 0x827F, 0x027A, 0x826B, 0x026E, 0x0264, 0x8261,
-            0x0220, 0x8225, 0x822F, 0x022A, 0x823B, 0x023E, 0x0234, 0x8231,
-            0x8213, 0x0216, 0x021C, 0x8219, 0x0208, 0x820D, 0x8207, 0x0202
-        ];
-
-        let mut crc_accum = 0x0000;
-        for j in 0..msg.len()
-        {
-            let i = ((((crc_accum >> 8) as u8) ^ msg[j]) & 0xFF) as usize;
-            crc_accum = (crc_accum << 8) ^ crc_table[i];
-        }
-    
-        crc_accum
-    }
 
 }
 
@@ -242,15 +197,6 @@ mod tests {
         let data: u32 = 0x00000200;
         dxl.write(1, ControlTable::GoalPosition.to_address(), &data.to_le_bytes());
         assert_eq!(*mock_uart.rx_buf, [0xFF, 0xFF, 0xFD, 0x00, 0x01, 0x09, 0x00, 0x03, 0x74, 0x00, 0x00, 0x02, 0x00, 0x00, 0xCA, 0x89]);    
-    }
-
-    #[test]    
-    fn crc() {
-        let mut mock_uart = MockSerial::new();
-        let dxl = DynamixelControl::new(&mut mock_uart);
-        let mut msg = Vec::<u8, 256>::new();
-        msg.extend([0xFF, 0xFF, 0xFD, 0x00, 0x01, 0x07, 0x00, 0x55, 0x00, 0x06, 0x04, 0x26].iter().cloned());
-        assert_eq!(dxl.calc_crc_value(&msg), 0x5D65);    
     }
 
     #[test]    
--- src/packet_handler.rs
+++ src/packet_handler.rs
@@ -2,7 +2,41 @@ use heapless::Vec;
 use crate::DynamixelControl;
 use crate::ControlTable;
 use crate::Instruction;
+use core::fmt;
 
+pub const MAX_PACKET_LEN: usize = 128;
+
+#[allow(dead_code)]
+pub enum Packet {
+    Header0,
+    Header1,
+    Header2,
+    Reserved,
+    Id,
+    LengthL,
+    LengthH,
+    Instruction,
+    Error,
+    Parameter0,
+}
+
+#[allow(dead_code)]
+impl Packet {
+    pub fn to_pos(&self) -> usize {
+        match self {
+            Packet::Header0 => 0,
+            Packet::Header1 => 1,
+            Packet::Header2 => 2,
+            Packet::Reserved => 3,
+            Packet::Id => 4,
+            Packet::LengthL => 5,
+            Packet::LengthH => 6,
+            Packet::Instruction => 7,
+            Packet::Error => 8,
+            Packet::Parameter0 => 8,
+        }
+    }
+}
 
 #[allow(dead_code)]
 #[derive(Debug)]
@@ -20,40 +54,59 @@ pub enum ErrorBit {
 #[allow(dead_code)]
 #[derive(Debug)]
 pub enum CommunicationResult {
-    CommSuccess,
-    CommPortBusy,
-    CommTxFail,
-    CommRxFail,
-    CommTxError,
-    CommRxWaiting,
-    CommRxTimeout,
-    CommRxCorrupt,
-    CommNotAvailable,
+    Success,
+    PortBusy,
+    TxFail,
+    RxFail,
+    TxError,
+    RxWaiting,
+    RxTimeout,
+    RxCorrupt,
+    NotAvailable,
+}
+impl fmt::Display for CommunicationResult {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            CommunicationResult::Success =>
+                write!(f, "[TxRxResult] Communication success."),
+                CommunicationResult::PortBusy =>
+                write!(f, "[TxRxResult] Port is in use!"),
+                CommunicationResult::TxFail =>
+                write!(f, "[TxRxResult] Failed transmit instruction packet!"),
+                CommunicationResult::RxFail =>
+                write!(f, "[TxRxResult] Failed get status packet from device!"),
+                CommunicationResult::TxError =>
+                write!(f, "[TxRxResult] Incorrect instruction packet!"),
+                CommunicationResult::RxWaiting =>
+                write!(f, "[TxRxResult] Now recieving status packet!"),
+                CommunicationResult::RxTimeout =>
+                write!(f, "[TxRxResult] There is no status packet!"),
+                CommunicationResult::RxCorrupt =>
+                write!(f, "[TxRxResult] Incorrect status packet!"),
+                CommunicationResult::NotAvailable =>
+                write!(f, "[TxRxResult] Protocol does not support This function!"),
+        }
+    }
 }
 
 
+#[allow(dead_code)]
 impl<'a> DynamixelControl<'a> {
-
-
-
 
     pub fn read(&mut self, id: u8, data_name: ControlTable, data: &mut [u8]) {
 
         let address = data_name.to_address();
         let size = data_name.to_size();
         let length: u16 = 1 + 2 + 2 + 2;     // instruction + adress + data length + crc
-        let mut msg = Vec::<u8, 256>::new();
-        msg.extend(self.make_msg_header().iter().cloned());
+        let mut msg = Vec::<u8, MAX_PACKET_LEN>::new();
+        msg.extend(self.reserve_msg_header().iter().cloned());
         msg.push(id).unwrap();
         msg.extend(length.to_le_bytes().iter().cloned());       // Set length temporary
         msg.push(Instruction::Read.to_value()).unwrap();
         msg.extend(address.to_le_bytes().iter().cloned());
         msg.extend(size.to_le_bytes().iter().cloned());
-        msg.extend(self.calc_crc_value(&msg).to_le_bytes().iter().cloned());
 
-        for m in msg {
-            self.uart.write_byte(m);
-        }
+        self.send_packet(msg);
 
         let mut status = Vec::<u8, 128>::new();
         let status_len = 4 + 1 + 2 + 1 + 1 + size + 2;     // header + id + length + instruction + err + param + crc
@@ -67,7 +120,166 @@ impl<'a> DynamixelControl<'a> {
         }
 
     }
+    fn add_stuffing(&mut self) {}
+    fn remove_stuffing(&mut self) {}
 
+    pub fn send_packet(&mut self, mut msg: Vec::<u8, MAX_PACKET_LEN>) -> CommunicationResult {
+        // make header
+        msg[0] = 0xFF;
+        msg[1] = 0xFF;
+        msg[2] = 0xFD;
+        msg[3] = 0x00;
+
+        // msg.extend(self.make_msg_header().iter().cloned());
+        // // msg.push(id).unwrap();
+        // msg.extend(length.to_le_bytes().iter().cloned());       // Set length temporary
+        // msg.push(Instruction::Ping.to_value()).unwrap();
+        
+        // add crc
+        msg.extend(self.calc_crc_value(&msg).to_le_bytes().iter().cloned());
+
+        for m in msg {
+            self.uart.write_byte(m);
+        }
+
+        CommunicationResult::Success
+    }
+    fn receive_packet(&mut self) -> CommunicationResult {
+        CommunicationResult::Success
+    }
+
+    // fn readTx(&mut self) {}
+    // fn readRx(&mut self) {}
+    // fn readTxRx(&mut self) {}
+    // fn read1ByteTx(&mut self) {}
+    // fn read1ByteRx(&mut self) {}
+    // fn read1ByteTxRx(&mut self) {}
+    // fn read2ByteTx(&mut self) {}
+    // fn read2ByteRx(&mut self) {}
+    // fn read2ByteTxRx(&mut self) {}
+    // fn read4ByteTx(&mut self) {}
+    // fn read4ByteRx(&mut self) {}
+    // fn read4ByteTxRx(&mut self) {}
+    // fn writeTxOnly(&mut self) {}
+    // fn writeTxRx(&mut self) {}
+    // fn write1ByteTx(&mut self) {}
+    // fn write1ByteTxRx(&mut self) {}
+    // fn write2ByteTx(&mut self) {}
+    // fn write2ByteTxRx(&mut self) {}
+    // regWriteTxOnly
+    // regWriteTxRx
+    // syncReadTx
+    // syncWriteTxOnly
+    // bulkReadTx
+    // bulkWriteTxOnly
+
+    pub fn calc_crc_value(&self, msg: &Vec<u8, MAX_PACKET_LEN>) -> u16 {
+        
+        let crc_table = [
+            0x0000, 0x8005, 0x800F, 0x000A, 0x801B, 0x001E, 0x0014, 0x8011,
+            0x8033, 0x0036, 0x003C, 0x8039, 0x0028, 0x802D, 0x8027, 0x0022,
+            0x8063, 0x0066, 0x006C, 0x8069, 0x0078, 0x807D, 0x8077, 0x0072,
+            0x0050, 0x8055, 0x805F, 0x005A, 0x804B, 0x004E, 0x0044, 0x8041,
+            0x80C3, 0x00C6, 0x00CC, 0x80C9, 0x00D8, 0x80DD, 0x80D7, 0x00D2,
+            0x00F0, 0x80F5, 0x80FF, 0x00FA, 0x80EB, 0x00EE, 0x00E4, 0x80E1,
+            0x00A0, 0x80A5, 0x80AF, 0x00AA, 0x80BB, 0x00BE, 0x00B4, 0x80B1,
+            0x8093, 0x0096, 0x009C, 0x8099, 0x0088, 0x808D, 0x8087, 0x0082,
+            0x8183, 0x0186, 0x018C, 0x8189, 0x0198, 0x819D, 0x8197, 0x0192,
+            0x01B0, 0x81B5, 0x81BF, 0x01BA, 0x81AB, 0x01AE, 0x01A4, 0x81A1,
+            0x01E0, 0x81E5, 0x81EF, 0x01EA, 0x81FB, 0x01FE, 0x01F4, 0x81F1,
+            0x81D3, 0x01D6, 0x01DC, 0x81D9, 0x01C8, 0x81CD, 0x81C7, 0x01C2,
+            0x0140, 0x8145, 0x814F, 0x014A, 0x815B, 0x015E, 0x0154, 0x8151,
+            0x8173, 0x0176, 0x017C, 0x8179, 0x0168, 0x816D, 0x8167, 0x0162,
+            0x8123, 0x0126, 0x012C, 0x8129, 0x0138, 0x813D, 0x8137, 0x0132,
+            0x0110, 0x8115, 0x811F, 0x011A, 0x810B, 0x010E, 0x0104, 0x8101,
+            0x8303, 0x0306, 0x030C, 0x8309, 0x0318, 0x831D, 0x8317, 0x0312,
+            0x0330, 0x8335, 0x833F, 0x033A, 0x832B, 0x032E, 0x0324, 0x8321,
+            0x0360, 0x8365, 0x836F, 0x036A, 0x837B, 0x037E, 0x0374, 0x8371,
+            0x8353, 0x0356, 0x035C, 0x8359, 0x0348, 0x834D, 0x8347, 0x0342,
+            0x03C0, 0x83C5, 0x83CF, 0x03CA, 0x83DB, 0x03DE, 0x03D4, 0x83D1,
+            0x83F3, 0x03F6, 0x03FC, 0x83F9, 0x03E8, 0x83ED, 0x83E7, 0x03E2,
+            0x83A3, 0x03A6, 0x03AC, 0x83A9, 0x03B8, 0x83BD, 0x83B7, 0x03B2,
+            0x0390, 0x8395, 0x839F, 0x039A, 0x838B, 0x038E, 0x0384, 0x8381,
+            0x0280, 0x8285, 0x828F, 0x028A, 0x829B, 0x029E, 0x0294, 0x8291,
+            0x82B3, 0x02B6, 0x02BC, 0x82B9, 0x02A8, 0x82AD, 0x82A7, 0x02A2,
+            0x82E3, 0x02E6, 0x02EC, 0x82E9, 0x02F8, 0x82FD, 0x82F7, 0x02F2,
+            0x02D0, 0x82D5, 0x82DF, 0x02DA, 0x82CB, 0x02CE, 0x02C4, 0x82C1,
+            0x8243, 0x0246, 0x024C, 0x8249, 0x0258, 0x825D, 0x8257, 0x0252,
+            0x0270, 0x8275, 0x827F, 0x027A, 0x826B, 0x026E, 0x0264, 0x8261,
+            0x0220, 0x8225, 0x822F, 0x022A, 0x823B, 0x023E, 0x0234, 0x8231,
+            0x8213, 0x0216, 0x021C, 0x8219, 0x0208, 0x820D, 0x8207, 0x0202
+        ];
+
+        let mut crc_accum = 0x0000;
+        for j in 0..msg.len()
+        {
+            let i = ((((crc_accum >> 8) as u8) ^ msg[j]) & 0xFF) as usize;
+            crc_accum = (crc_accum << 8) ^ crc_table[i];
+        }
+    
+        crc_accum
+    }
 
 }
 
+#[cfg(test)]
+mod tests {
+    use heapless::Deque;
+    use heapless::Vec;
+    use crate::DynamixelControl;
+    use crate::Instruction;
+    use crate::ControlTable;
+    use crate::packet_handler::MAX_PACKET_LEN;
+
+    pub struct MockSerial {
+        rx_buf: Vec<u8, 256>,
+        tx_buf: Deque<u8, 256>,
+    }
+    impl MockSerial {
+        pub fn new() -> Self {
+            Self { 
+                rx_buf: Vec::<u8, 256>::new(), 
+                tx_buf: Deque::<u8, 256>::new(), 
+            }
+        }
+    }
+    impl crate::Interface for MockSerial {
+        fn write_byte(&mut self, data: u8) {
+            
+            self.rx_buf.push(data).unwrap();
+
+            // For test ping
+            if self.rx_buf.len() > 7 && self.rx_buf[7] == Instruction::Ping.to_value() {
+                // ID1(XM430-W210) : For Model Number 1030(0x0406), Version of Firmware 38(0x26)
+                // Instruction Packet ID : 1
+                let res = [0xFF, 0xFF, 0xFD, 0x00, 0x01, 0x07, 0x00, 0x55, 0x00, 0x06, 0x04, 0x26, 0x65, 0x5D];
+                for data in res{
+                    self.tx_buf.push_back(data).unwrap();
+                }
+            }
+            // For test read
+            if self.rx_buf.len() > 7 && self.rx_buf[7] == Instruction::Read.to_value() {
+                // ID1(XM430-W210) : Present Position(132, 0x0084, 4[byte]) = 166(0x000000A6)
+                let res = [0xFF, 0xFF, 0xFD, 0x00, 0x01, 0x07, 0x00, 0x55, 0x00, 0x06, 0x04, 0x26, 0x65, 0x5D];
+                for data in res{
+                    self.tx_buf.push_back(data).unwrap();
+                }
+            }         
+        }
+        fn read(&mut self) -> Option<u8> {
+            self.tx_buf.pop_front()
+        }
+    }
+
+    #[test]    
+    fn crc() {
+        let mut mock_uart = MockSerial::new();
+        let dxl = DynamixelControl::new(&mut mock_uart);
+        let mut msg = Vec::<u8, MAX_PACKET_LEN>::new();
+        msg.extend([0xFF, 0xFF, 0xFD, 0x00, 0x01, 0x07, 0x00, 0x55, 0x00, 0x06, 0x04, 0x26].iter().cloned());
+        assert_eq!(dxl.calc_crc_value(&msg), 0x5D65);    
+    }
+
+
+
+}
